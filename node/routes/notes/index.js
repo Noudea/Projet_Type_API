a3c6be5d6f5745ab320c2123c1058c65c6cce41d
Nicolas Oudea <oudea.nicolas@gmail.com>
2020-11-13 09:12:23 +0100

    add POST and DELETE to notes

--- node/routes/notes/index.js
+++ node/routes/notes/index.js
@@ -1,10 +1,71 @@
 const router = require('express').Router()
 
+let notes =
+[
+    {
+        id : '1',
+        title : 'Ma note 1',
+        description : 'Ma description 1',
+        isEnabled : true,
+        isFavorite: false
+    },
+    {
+        id: '2',
+        title: 'Ma note 2',
+        description: 'Ma description 2',
+        isEnabled: true,
+        isFavorite: false
+    },
+    {
+        id: '3',
+        title: 'Ma note 3',
+        description: 'Ma description 3',
+        isEnabled: true,
+        isFavorite: false
+    }
+]
 //Nous sommes déjà dans /notes
 router.route('/') //localhost:PORT/notes/
-.get((req,res) =>
+.get((req,res) => //récupération de la liste de note
 {
-    res.send('ma liste de note')
+    res.send(notes)
 })
+.post((req,res) => {
+console.log('body',req.body);
+const title = req.body.title
+const description = req.body.description
+
+//On teste si la data recue n'est pas nulle
+if(!title)
+{
+    res.status(500).send('Le titre est manquant')
+}
+else if (!description) 
+{
+    res.status(500).send('La description est manquante')
+} else
+{
+    //on ajoute la note a la liste de note
+    notes.push(
+        {
+            id : Math.random().toString(36).substr(2,9),
+            title: title,
+            description : description,
+            isEnabled : true,
+            isFavorite : false
+        })
+    res.send(notes)
+}
+})
+
+.delete((req,res) => {
+    const id = req.body.id
+    index = notes.findIndex(element => element.id === id);
+    console.log(index);
+    notes.splice(index,1)
+    res.send(notes)
+})
+
+
 
 module.exports = router
--- node/server.js
+++ node/server.js
@@ -1,18 +1,32 @@
+//Init env
 require('dotenv').config()
+
+//Imports
 const express = require('express')
+const bodyParser = require('body-parser')
 const Logger = require('./tools/logger')
+
 const app = express()
+//Réglages du port d'ecoutes
 const port = process.env.PORT || 3000
 const router = express.Router()
 
+//Mise en place du bodyParser afin d'obtenir req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+//Mise en place du logger
 app.use(Logger)
-app.use(router)
 
+app.use(router)
 app.use('/',require('./routes'))
 app.use('/notes',require('./routes/notes'))
+
+
 app.listen(port, () => {
     console.log('====================================');
     console.log(`Example app listening at http://localhost:${port}`);
     console.log('====================================');
 })
+
+
 
